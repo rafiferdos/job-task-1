36048c29e39c22fc50421944354970d9c9a0f4e3
Rafi Ferdos <rafiferdos@gmail.com>
2024-07-12 18:45:59 +0600

    chore: Update Hero component with user details

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import bg from "@/assets/images/bg.jpg";
+import UserDetails from "./user-details";
 const Hero = () => {
   return (
     <div className="w-full flex items-center justify-center shrink-0 mx-0 px-0 flex-col">
-      <div className="w-full overflow-hidden h-80 md:h-[550px]">
-        <Image src={bg} className="w-full" objectFit="contain" alt="bg" />
+      <div className="w-full overflow-hidden md:h-[550px]">
+        <Image src={bg} className="w-full" objectFit="cover" alt="bg" />
       </div>
-      <div className="">d</div>
+      <div className="w-full">
+        <UserDetails />
+      </div>
     </div>
   );
 };
