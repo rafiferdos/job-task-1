86d63119e4a8d628beb1beeb4ff26b93f33b3e1a
Rafi Ferdos <rafiferdos@gmail.com>
2024-07-12 18:51:41 +0600

    chore: Update Hero component with user details and improve layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="w-full flex items-center justify-center shrink-0 mx-0 px-0 flex-col">
       <div className="w-full overflow-hidden md:h-[550px]">
-        <Image src={bg} className="w-full" objectFit="cover" alt="bg" />
+        <Image src={bg} className="w-full h-full" objectFit="contain" alt="bg" />
       </div>
       <div className="w-full">
         <UserDetails />
--- src/components/user-details.tsx
+++ src/components/user-details.tsx
@@ -5,26 +5,26 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { GiConsoleController } from "react-icons/gi";
 const UserDetails = () => {
   return (
-    <div className="flex items-start gap-8 lg:w-1/2 mx-auto p-8 justify-center">
+    <div className="flex items-start gap-2 md:gap-8 lg:w-1/2 mx-auto p-2 md:p-8 justify-center">
       <Image
         src={profilePic}
-        className="-mt-20 md:-mt-24 w-20 md:w-44"
+        className="-mt-12 md:-mt-24 w-20 md:w-44"
         alt="profile"
       />
       <div className="flex items-start justify-center gap-4">
         <div className="flex flex-col gap-3">
           <div className="flex gap-2 items-center">
-            <h3 className="font-bold text-xs md:text-xl">John Doe</h3>
-            <h3 className="font-extrabold">·</h3>
-            <h4 className="text-green-500">@johndoe</h4>
+            <h3 className="font-bold text-sm md:text-2xl">John Doe</h3>
+            <h3 className="font-extrabold text-xs md:text-xl">·</h3>
+            <h4 className="text-green-500 text-xs md:text-xl">@johndoe</h4>
           </div>
           <div className="flex items-center justify-center gap-3">
 
           <div className="flex flex-col gap-1">
-            <h4 className="flex items-center gap-2">
+            <h4 className="flex items-center gap-2 text-sm md:text-base">
               Astrographer <BsFillMoonStarsFill />
             </h4>
-            <h4 className="flex items-center gap-2">
+            <h4 className="flex items-center gap-2 text-sm md:text-base">
               Gamer <GiConsoleController />
             </h4>
           </div>
